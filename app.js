98c5ee68ae97b4857a0caace0be7ad3a2a5d0d8c
Dave Rowan <drowan99@gmail.com>
2016-06-05 16:43:23 -0400

    Moved views into inline templates using ui.router and ui-view.
    Added comments to posts, and a posts view to peruse and add comments to posts.
    
    Signed-off-by: Dave Rowan <drowan99@gmail.com>

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider){
+
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+
+	$urlRouterProvider.otherwise('/home')
+
+}])
 
 .factory('posts', [function(){
 
 	var o = {
 		posts: [
-			{title: 'post 1', upvotes: 5},
-			{title: 'post 2', upvotes: 2},
-			{title: 'post 3', upvotes: 15},
-			{title: 'post 4', upvotes: 9},
-			{title: 'post 5', upvotes: 4}
+			{title: 'post 1', upvotes: 5, comments: []},
+			{title: 'post 2', upvotes: 2, comments: []},
+			{title: 'post 3', upvotes: 15, comments: []},
+			{title: 'post 4', upvotes: 9, comments: []},
+			{title: 'post 5', upvotes: 4, comments: []}
 		]
 	};
 
@@ -29,14 +50,37 @@ function($scope, posts){
 		$scope.posts.push({
 			title: $scope.title, 
 			link: $scope.link, 
-			upvotes: 0
+			upvotes: 0,
+			comments: []
 		});
 		$scope.title = '';
 		$scope.link = '';
 	}
 
 	$scope.incrementUpvotes = function(post) {
-		post.upvotes += 1
+		post.upvotes += 1;
 	}
 
+}])
+
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
+	$scope.post = posts.posts[$stateParams.id];
+
+	$scope.addComment = function() {
+		if(!$scope.body || $scope.body === '') {return;}
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+		$scope.body = '';
+	}
+
+	$scope.incrementUpvotes = function(comment) {
+		comment.upvotes += 1;
+	}
 }])
